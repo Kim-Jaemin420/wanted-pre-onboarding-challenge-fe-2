99c38b4cbdb0262af5dc9e1c7cc4df51714a6f8b
Kim-Jaemin420 <jany240@naver.com>
2023-01-13 15:51:40 +0900

    reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
 import App from './App';
+import { reset } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 );
