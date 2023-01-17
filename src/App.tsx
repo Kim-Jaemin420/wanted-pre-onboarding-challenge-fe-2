575e7b4664a490f6250e842c5e14a80b65218912
Kim-Jaemin420 <jany240@naver.com>
2023-01-17 17:15:03 +0900

    Feat: 로그인, 회원가입 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Signin, Signup } from './pages';
+import Todos from './pages/Todos/Todos';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Todos />} />
+          <Route path="/auth/signin" element={<Signin />} />
+          <Route path="/auth/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
