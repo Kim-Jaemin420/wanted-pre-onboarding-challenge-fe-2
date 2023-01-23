2adac799317a8d41d6d227a7ac690451585ca6e5
Kim-Jaemin420 <jany240@naver.com>
2023-01-23 17:04:27 +0900

    Modify: 잘못된 환경 변수 수정

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -2,14 +2,13 @@ import { HTTP_METHODS } from '@/consts';
 import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
 
 const createApiMethod =
-  (axiosInstance: AxiosInstance, method: Method) =>
-  (config: AxiosRequestConfig) => {
+  (axiosInstance: AxiosInstance, method: Method) => (config: AxiosRequestConfig) => {
     axiosInstance.interceptors.response.use((response) => {
       return response;
     });
@@ -20,10 +19,12 @@ const createApiMethod =
     });
   };
 
-export default {
+const api = {
   get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
   post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
   patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
   put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
   delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
 };
+
+export default api;
