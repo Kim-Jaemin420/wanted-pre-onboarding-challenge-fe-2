4190285f1d905b461f06612ca4b09718530e1331
Kim-Jaemin420 <jany240@naver.com>
2023-01-23 18:47:59 +0900

    Chore: http error 상수 추가

--- src/consts/network.ts
+++ src/consts/network.ts
@@ -7,3 +7,15 @@ export const HTTP_METHODS: Record<string, Method> = {
   PUT: 'put',
   DELETE: 'delete',
 };
+
+export const HTTP_ERRORS = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  PAYMENT_REQUIRED: 402,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+  BAD_GATEWAY: 502,
+};
