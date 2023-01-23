5965e83649fd9628f8a37b4439b88923d9bdc12d
Kim-Jaemin420 <jany240@naver.com>
2023-01-23 15:12:05 +0900

    Feat: email, password를 validate 한 후 에러 메시지 설정

---
+++ src/pages/Signup/Signup.tsx
@@ -0,0 +1,117 @@
+import { useState } from 'react';
+import { css } from '@emotion/react';
+import { SignupForm } from '@/components/index';
+import { SignupFieldError } from '@/types';
+
+const ERROR_TYPE = {
+  INVALID_EMAIL: 'invalidEmail',
+  INVALID_PASSWORD: 'invalidPassword',
+  DIFFERENT_PASSWORD: 'differentPassword',
+} as const;
+
+const ERROR_MESSAGE = {
+  invalidEmail: '이메일 형식에 맞게 작성해주세요.',
+  invalidPassword: '비밀번호는 8자리 이상 입력해주세요.',
+  differentPassword: '동일한 비밀번호를 작성해주세요.',
+};
+
+function Signup() {
+  const [signupFieldErrorMessage, setSignupFieldErrorMessage] = useState<SignupFieldError>({
+    emailErrorMessage: null,
+    passwordErrorMessage: null,
+    passwordConfirmErrorMessage: null,
+  });
+
+  const checkEmailErrorMessage = (email: string) => {
+    const EMAIL_REGEX =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    if (!EMAIL_REGEX.test(email)) {
+      return ERROR_MESSAGE[ERROR_TYPE.INVALID_EMAIL];
+    }
+
+    return null;
+  };
+
+  const checkPasswordsErrorMessage = (
+    password: string,
+    passwordConfirm: string
+  ): { passwordErrorMessage: string | null; passwordConfirmErrorMessage: string | null } => {
+    if (0 < password.length && password.length < 8 && !passwordConfirm)
+      return {
+        passwordErrorMessage: '8자리 이상 입력해주세요',
+        passwordConfirmErrorMessage: null,
+      };
+
+    if (passwordConfirm.length > 0 && password !== passwordConfirm) {
+      return {
+        passwordErrorMessage: '동일한 비밀번호를 입력해주세요.',
+        passwordConfirmErrorMessage: '동일한 비밀번호를 입력해주세요.',
+      };
+    }
+
+    return {
+      passwordErrorMessage: null,
+      passwordConfirmErrorMessage: null,
+    };
+  };
+
+  const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { emailField, passwordField, passwordConfirmField } = event.currentTarget;
+
+    const email = emailField.value;
+    const password = passwordField.value;
+    const passwordConfirm = passwordConfirmField.value;
+
+    console.log(emailField.value, passwordField.value, passwordConfirmField);
+  };
+
+  const handleChangeForm = (event: React.FormEvent<HTMLFormElement>) => {
+    const target = event.target as HTMLInputElement;
+
+    const { emailField, passwordField, passwordConfirmField } = event.currentTarget;
+
+    const emailErrorMessage = checkEmailErrorMessage(emailField.value);
+    const { passwordErrorMessage, passwordConfirmErrorMessage } = checkPasswordsErrorMessage(
+      passwordField.value,
+      passwordConfirmField.value
+    );
+
+    if (target.name === 'emailField') {
+      setSignupFieldErrorMessage((previousError) => ({
+        ...previousError,
+        emailErrorMessage,
+      }));
+    }
+
+    if (target.name === 'passwordField' || target.name === 'passwordConfirmField') {
+      setSignupFieldErrorMessage((previousError) => ({
+        ...previousError,
+        passwordErrorMessage,
+        passwordConfirmErrorMessage,
+      }));
+    }
+  };
+
+  return (
+    <div
+      css={css`
+        height: 100vh;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <SignupForm
+        handleSubmitForm={handleSubmitForm}
+        handleChangeForm={handleChangeForm}
+        error={signupFieldErrorMessage}
+      />
+    </div>
+  );
+}
+
+export default Signup;
