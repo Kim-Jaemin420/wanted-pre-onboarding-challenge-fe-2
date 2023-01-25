a152a497191d853f16577750377e5de93308eb9f
Kim-Jaemin420 <jany240@naver.com>
2023-01-25 16:39:08 +0900

    Style: 불필요한 스타일 제거, input 컨테이너 스타일 추가

--- src/components/SigninForm/signinFormStyle.ts
+++ src/components/SigninForm/signinFormStyle.ts
@@ -1,14 +1,8 @@
 import styled from '@emotion/styled';
 import { TextField, Button } from '@mui/material';
 
-export const IdField = styled(TextField)`
-  width: 100%;
-  margin: 20px 0;
-`;
-
-export const PasswordField = styled(TextField)`
-  width: 100%;
-  margin-bottom: 20px;
+export const InputContainer = styled.div`
+  height: 30px;
 `;
 
 export const SubmitButton = styled(Button)`
