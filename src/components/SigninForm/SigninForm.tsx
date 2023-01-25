5f8407553ffad03e223cb5e79ae602f56612f743
Kim-Jaemin420 <jany240@naver.com>
2023-01-25 16:41:51 +0900

    Modify: 변경된 스타일 적용

--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -1,31 +1,39 @@
-import { css } from '@emotion/react';
-import { a11yHidden } from '@/styles';
-import { IdField, PasswordField, SubmitButton } from './signinFormStyle';
+import { TextField } from '@mui/material';
+import { InputContainer, SubmitButton } from './signinFormStyle';
 
-interface Props {}
+interface Props {
+  handleSubmit: React.FormEventHandler<HTMLFormElement>;
+}
 
-function SigninForm(props: Props) {
-  const {} = props;
-
+function SigninForm({ handleSubmit }: Props) {
   return (
-    <form>
-      <legend css={a11yHidden}>가입하기</legend>
-      <IdField
-        required
-        variant="outlined"
-        id="email"
-        label="Required"
-        placeholder="email"
-      />
-      <PasswordField
-        required
-        variant="outlined"
-        id="password"
-        name="password"
-        label="Password"
-        type="password"
-      />
-      <SubmitButton variant="outlined">로그인</SubmitButton>
+    <form onSubmit={handleSubmit}>
+      <legend>로그인 하기</legend>
+      <InputContainer>
+        <TextField
+          required
+          variant="outlined"
+          id="email"
+          name="emailField"
+          label="email"
+          placeholder="email"
+          fullWidth
+        />
+      </InputContainer>
+      <InputContainer>
+        <TextField
+          required
+          variant="outlined"
+          id="password"
+          name="passwordField"
+          label="Password"
+          type="password"
+          fullWidth
+        />
+      </InputContainer>
+      <SubmitButton type="submit" variant="outlined">
+        로그인
+      </SubmitButton>
     </form>
   );
 }
