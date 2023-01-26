e82d0111a141e9269821d63d0b85091c2018accd
Kim-Jaemin420 <jany240@naver.com>
2023-01-26 10:10:32 +0900

    Feat: AuthContext를 전역에서 사용할 수 있게 provider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global } from '@emotion/react';
+import { AuthProvider } from './context';
 import App from './App';
 import { reset } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Global styles={reset} />
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
