8e26051346b86b46c5b44eef189f1339646cc07f
Kim-Jaemin420 <jany240@naver.com>
2023-01-27 22:28:16 +0900

    Feat: useForm 훅을 사용하여 로그인 api 연결

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,14 +1,57 @@
-import { SigninForm } from '@/components/index';
+import { useContext } from 'react';
+import { AxiosError } from 'axios';
+import { css } from '@emotion/react';
+import { AuthContext } from '@/context';
+import { SigninForm } from '@/components';
+import { useForm } from '@/hooks';
+import { signin } from '@/apis';
+import { HTTP_ERRORS } from '@/consts';
 
 function Signin() {
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { setToken } = useContext(AuthContext);
 
-    const { emailField, passwordField } = event.currentTarget;
-    console.log(emailField.value, passwordField.value);
+  const login = async () => {
+    try {
+      const {
+        data: { message, token },
+      } = await signin(formInputValues);
+
+      setToken(token);
+
+      alert(message);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === HTTP_ERRORS.BAD_REQUEST) {
+          alert(error.response.data.details);
+        }
+
+        console.error(error);
+      }
+    }
   };
 
-  return <SigninForm handleSubmit={handleSubmit} />;
+  const { formInputValues, errors, handleChangeForm, handleSubmitForm } = useForm({
+    initialFormValues: { email: '', password: '' },
+    submittingFunction: login,
+  });
+
+  return (
+    <div
+      css={css`
+        height: 100vh;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <SigninForm
+        handleChangeForm={handleChangeForm}
+        handleSubmitForm={handleSubmitForm}
+        errors={errors}
+      />
+    </div>
+  );
 }
 
 export default Signin;
