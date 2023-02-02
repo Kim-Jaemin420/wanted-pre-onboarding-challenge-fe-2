5d2177756a428c87aa302679013c23cc91bc4d50
Kim-Jaemin420 <jany240@naver.com>
2023-02-03 03:25:22 +0900

    Feat: todo 생성 api 요청

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { AxiosError } from 'axios';
 import { css } from '@emotion/react';
+import { createTodo } from '@/apis';
 import { a11yHidden } from '@/styles';
 import { TodoDetailInput, AddButton, TodoInput } from './todoFormStyle';
 
 function TodoForm() {
   const handleSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const { todoTitle, content } = event.currentTarget;
+
+    try {
+      await createTodo({
+        title: todoTitle.value,
+        content: content.value,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.error(error);
+      }
+    }
   };
 
   return (
@@ -18,16 +33,16 @@ function TodoForm() {
       <legend css={a11yHidden}>todo 작성하기</legend>
       <div
         css={css`
-          padding: 0 1.5rem;
+          padding: 0 3rem;
         `}
       >
-        <TodoInput label="오늘 할 일이 무엇인가요?" name="title" variant="outlined" fullWidth />
+        <TodoInput label="오늘 할 일이 무엇인가요?" name="todoTitle" variant="outlined" fullWidth />
         <TodoDetailInput
           id="outlined-multiline-static"
           label="할 일에 대한 설명을 적어주세요."
           name="content"
           multiline
-          rows={15}
+          rows={18}
           variant="outlined"
           fullWidth
         />
