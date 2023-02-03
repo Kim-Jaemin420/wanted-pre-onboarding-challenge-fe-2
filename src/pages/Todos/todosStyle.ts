0bba333a1130bf91755724431da7607b617317d6
Kim-Jaemin420 <jany240@naver.com>
2023-02-03 21:19:00 +0900

    Style: style 작성 방식 변경
    styled.div() => styled.div``;

--- src/pages/Todos/todosStyle.ts
+++ src/pages/Todos/todosStyle.ts
@@ -1,28 +1,28 @@
 import styled from '@emotion/styled';
 
-export const TodoFormContainer = styled.div((props) => ({
-  width: '50%',
-  height: '60rem',
-  border: `0.03rem solid ${props.theme.colors.primary}`,
-  borderRight: 0,
-  borderBottomLeftRadius: '0.5rem',
-  borderTopLeftRadius: '0.5rem',
-}));
+export const TodoFormContainer = styled.div`
+  width: 50%;
+  height: 60rem;
+  border: 0.03rem solid ${(props) => props.theme.colors.primary};
+  border-right: 0;
+  border-bottom-left-radius: 0.5rem;
+  border-top-left-radius: 0.5rem;
+`;
 
-export const TodosContainer = styled.div((props) => ({
-  'width': '50%',
-  'height': '60rem',
-  'border': `0.03rem solid ${props.theme.colors.primary}`,
-  'borderLeft': 0,
-  'borderBottomRightRadius': '0.5rem',
-  'borderTopRightRadius': '0.5rem',
-  '&:before': {
-    content: "''",
-    position: 'absolute',
-    top: '50%',
-    transform: 'translateY(-50%)',
-    width: '0.1rem',
-    height: '55rem',
-    backgroundColor: `${props.theme.colors.neutrual}`,
+export const TodosContainer = styled.div`
+  width: 50%;
+  height: 60rem;
+  border: 0.03rem solid ${(props) => props.theme.colors.primary};
+  borderLeft: 0;
+  borderBottomRightRadius: 0.5rem;
+  borderTopRightRadius: 0.5rem;
+  &:before: {
+    content: "";
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 0.1rem;
+    height: 55rem;
+    backgroundColor: ${(props) => props.theme.colors.neutrual};
   },
-}));
+`;
