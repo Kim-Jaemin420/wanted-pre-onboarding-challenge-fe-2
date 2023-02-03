61f744324fea2a3ed671de596b380497c3f27d10
Kim-Jaemin420 <jany240@naver.com>
2023-02-03 21:27:27 +0900

    Feat: todo list ui 구현

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,19 @@
+import { TodoItem } from '@/components';
+import { ListContainer } from './todoListStyle';
+
+function TodoList() {
+  return (
+    <ListContainer>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </ListContainer>
+  );
+}
+
+export default TodoList;
---
+++ src/components/TodoList/todoListStyle.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+import { List } from '@mui/material';
+
+export const ListContainer = styled(List)`
+  overflow-y: scroll;
+  height: 60rem;
+`;
