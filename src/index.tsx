0b60b3e32a18c48e876e8f526aee4be6a1dba987
Kim-Jaemin420 <jany240@naver.com>
2023-02-08 02:14:03 +0900

    Feat: query client provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@mui/material';
 import { AuthProvider } from './context';
 import App from './App';
 import { reset, muiTheme, theme } from './styles';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Global styles={reset} />
-    <ThemeProvider theme={{ ...muiTheme, ...theme }}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Global styles={reset} />
+      <ThemeProvider theme={{ ...muiTheme, ...theme }}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
