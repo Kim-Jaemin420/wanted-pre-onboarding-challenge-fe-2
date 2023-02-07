02652f47e697b58ef8f527b1c29ec82348dac34c
Kim-Jaemin420 <jany240@naver.com>
2023-02-07 15:02:29 +0900

    Feat: 리스트 삭제 버튼 클릭시 api 연결

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,21 +1,35 @@
-import { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { useNavigate } from 'react-router';
 import { ListItemText } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteTodo } from '@/apis';
 import { TodoResponse } from '@/types';
 import { PAGE_ROUTE } from '@/consts';
 import { ButtonContainer, Item, ItemButton, ItemContent, ItemTitle } from './todoItemStyle';
 
 interface Props {
   todo: TodoResponse;
+  setTodos: Dispatch<SetStateAction<TodoResponse[]>>;
 }
 
-function TodoItem({ todo }: Props) {
+function TodoItem({ todo, setTodos }: Props) {
   const navigate = useNavigate();
 
   const handleClickTodoItem = () => {
     navigate(`${PAGE_ROUTE.TODOS}/${todo.id}`);
+  };
+
+  const handleClickDeleteButton = async () => {
+    const { id } = todo;
+
+    try {
+      await deleteTodo({ id });
+
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -30,7 +44,7 @@ function TodoItem({ todo }: Props) {
           <EditIcon />
           수정
         </ButtonContainer>
-        <ButtonContainer>
+        <ButtonContainer name="deleteButton" onClick={handleClickDeleteButton}>
           <DeleteIcon />
           삭제
         </ButtonContainer>
