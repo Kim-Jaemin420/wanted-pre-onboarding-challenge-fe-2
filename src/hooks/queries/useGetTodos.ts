e58fc3fe85c688af98f2ac978001994c8afa84f4
Kim-Jaemin420 <jany240@naver.com>
2023-02-22 14:58:50 +0900

    Modify: query key를 변수로 관리

--- src/hooks/queries/useGetTodos.ts
+++ src/hooks/queries/useGetTodos.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getTodos } from '@/apis';
+import { TODO_KEYS } from '@/consts/query';
 
 const useGetTodos = () => {
   const { data, isLoading, error } = useQuery({
-    queryKey: ['todos'],
+    queryKey: TODO_KEYS.lists(),
     queryFn: getTodos,
     staleTime: 10000,
   });
