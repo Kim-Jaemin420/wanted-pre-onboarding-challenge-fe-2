d84e049ac44c4c857dcf8809139e5bc5f8b8c8f8
Kim-Jaemin420 <jany240@naver.com>
2023-02-23 17:39:39 +0900

    Modify: todo key의 detail의 id 타입 변경
    id 타입을 string에서 number로 변경

--- src/consts/query.ts
+++ src/consts/query.ts
@@ -3,5 +3,5 @@ export const TODO_KEYS = {
   lists: () => [...TODO_KEYS.all, 'list'] as const,
   list: (filters: string) => [...TODO_KEYS.lists(), { filters }] as const,
   details: () => [...TODO_KEYS.all, 'detail'] as const,
-  detail: (id: number) => [...TODO_KEYS.details(), id] as const,
+  detail: (id: string) => [...TODO_KEYS.details(), id] as const,
 };
