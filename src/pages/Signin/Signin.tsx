c0f04566e9f21f0099ff8bc8a19081d8051dd6a9
Kim-Jaemin420 <jany240@naver.com>
2023-02-28 16:22:10 +0900

    Modify: api 커스텀 훅 적용

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,38 +1,13 @@
-import { useContext } from 'react';
-import { AxiosError } from 'axios';
 import { css } from '@emotion/react';
-import { AuthContext } from '@/context';
 import { SigninForm } from '@/components';
-import { useForm } from '@/hooks';
-import { signin } from '@/apis';
-import { HTTP_ERRORS } from '@/consts';
+import { useForm, usePostLogin } from '@/hooks';
 
 function Signin() {
-  const { setToken } = useContext(AuthContext);
+  const postLogin = usePostLogin();
 
-  const login = async () => {
-    try {
-      const {
-        data: { message, token },
-      } = await signin(formInputValues);
-
-      setToken(token);
-
-      alert(message);
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === HTTP_ERRORS.BAD_REQUEST) {
-          alert(error.response.data.details);
-        }
-
-        console.error(error);
-      }
-    }
-  };
-
-  const { formInputValues, errors, handleChangeForm, handleSubmitForm } = useForm({
+  const { errors, handleChangeForm, handleSubmitForm } = useForm({
     initialFormValues: { email: '', password: '' },
-    submittingFunction: login,
+    submittingFunction: postLogin.mutate,
   });
 
   return (
