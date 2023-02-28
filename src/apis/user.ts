64a731ef267eae198f4fb78a77e2aac688c3ad16
Kim-Jaemin420 <jany240@naver.com>
2023-02-28 16:01:47 +0900

    Modify: user api response data, catch 에러 추가

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,14 +1,21 @@
 import { User } from '@/types';
+import { throwError } from '@/utils';
 import api from './base';
 
 export const signup = ({ email, password }: User) =>
-  api.post({
-    url: '/users/create',
-    data: { email, password },
-  });
+  api
+    .post({
+      url: '/users/create',
+      data: { email, password },
+    })
+    .then((response) => response.data)
+    .catch(throwError);
 
 export const signin = ({ email, password }: User) =>
-  api.post({
-    url: '/users/login',
-    data: { email, password },
-  });
+  api
+    .post({
+      url: '/users/login',
+      data: { email, password },
+    })
+    .then((response) => response.data)
+    .catch(throwError);
