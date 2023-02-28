6150d441b5d8806ff584203e80cb9da126b5b6a1
Kim-Jaemin420 <jany240@naver.com>
2023-02-28 16:22:39 +0900

    Modify: 커스텀 훅 사용으로 불필요해진 로직 삭제

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,38 +1,13 @@
-import { useContext } from 'react';
-import { AxiosError } from 'axios';
 import { css } from '@emotion/react';
-import { AuthContext } from '@/context';
-import { signup } from '@/apis';
-import { useForm } from '@/hooks';
-import { HTTP_ERRORS } from '@/consts';
+import { useForm, usePostSignup } from '@/hooks';
 import { SignupForm } from '@/components';
 
 function Signup() {
-  const { setToken } = useContext(AuthContext);
+  const postSignup = usePostSignup();
 
-  const register = async () => {
-    try {
-      const {
-        data: { message, token },
-      } = await signup(formInputValues);
-
-      setToken(token);
-
-      alert(message);
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === HTTP_ERRORS.CONFLICT) {
-          alert(error.response.data.details);
-        }
-
-        console.error(error);
-      }
-    }
-  };
-
-  const { formInputValues, errors, handleChangeForm, handleSubmitForm } = useForm({
+  const { errors, handleChangeForm, handleSubmitForm } = useForm({
     initialFormValues: { email: '', password: '', passwordConfirm: '' },
-    submittingFunction: register,
+    submittingFunction: postSignup.mutate,
   });
 
   return (
