48c9ac76afef6fb90bc37e12cdc829a847bad64d
Kim-Jaemin420 <jany240@naver.com>
2023-02-28 16:09:44 +0900

    Modify: todos data가 없는 경우를 위해 옵셔널 체이닝 사용

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,7 +8,7 @@ function TodoList() {
 
   return (
     <ListContainer>
-      {todos.map((todo: TodoResponse) => (
+      {todos?.map((todo: TodoResponse) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </ListContainer>
