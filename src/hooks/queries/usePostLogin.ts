cdb1159600da815e7b870adfe213638d38f5c9ea
Kim-Jaemin420 <jany240@naver.com>
2023-03-03 18:52:26 +0900

    Feat: login 커스텀 훅 response를 사용하여 모달 적용

--- src/hooks/queries/usePostLogin.ts
+++ src/hooks/queries/usePostLogin.ts
@@ -1,28 +1,28 @@
-import { useContext } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { AuthContext } from '@/context';
 import { signin } from '@/apis';
+import { useModal } from '@/hooks';
 import { HTTP_ERRORS } from '@/consts';
 import { User } from '@/types';
 
 const usePostLogin = () => {
-  const { setToken } = useContext(AuthContext);
+  const { openModal, setModalMessage } = useModal();
 
   return useMutation({
     mutationFn: ({ email, password }: User) => signin({ email, password }),
     onError: (error) => {
       if (error instanceof AxiosError) {
         if (error.response?.status === HTTP_ERRORS.BAD_REQUEST) {
-          alert(error.response.data.details);
+          openModal();
+          setModalMessage(error.response.data.details);
         }
 
         console.error(error);
       }
     },
     onSuccess: (data) => {
-      setToken(data.token);
-      alert(data.message);
+      openModal();
+      setModalMessage(data.message);
     },
   });
 };
