2a82606123191de97ebd610b9e647c0b88118719
Kim-Jaemin420 <jany240@naver.com>
2023-03-03 18:16:58 +0900

    Feat: todo 수정에서 모달 추가

--- src/hooks/queries/usePatchTodo.ts
+++ src/hooks/queries/usePatchTodo.ts
@@ -1,15 +1,23 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateTodo } from '@/apis';
+import { useModal } from '@/hooks';
 import { UpdateTodoRequest } from '@/types';
 import { TODO_KEYS } from '@/consts';
 
 const usePatchTodo = () => {
   const queryClient = useQueryClient();
+  const { openModal, setModalMessage } = useModal();
 
   return useMutation({
     mutationFn: ({ id, title, content }: UpdateTodoRequest) => updateTodo({ id, title, content }),
+    onError: () => {
+      openModal();
+      setModalMessage('수정이 실패하였습니다.');
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: TODO_KEYS.lists() });
+      openModal();
+      setModalMessage('수정되었습니다.');
     },
   });
 };
